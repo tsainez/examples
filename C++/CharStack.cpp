05e3c24117e69d74a485784dbd130b1f6242e59b
Anthony Sainez <tsainez@gmail.com>
2019-03-07 08:56:39 -0800

    Create CharStack.cpp

---
+++ C++/CharStack.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class CharStack {
+private:
+    int size;
+    int top;
+    char *stackArray;
+public:
+    CharStack(int newSize) {
+        size = newSize;
+        stackArray = new char[size];
+        top = -1; // no items yets
+    }
+    
+    =
+    void push(char newLetter) {
+        stackArray[++top] = newLetter;
+    }
+    
+    char pop() {
+        return stackArray[top--];
+    }
+    
+    char peek() {
+        return stackArray[top];
+    }
+    
+    bool isEmpty() {
+        return (top == -1);
+    }
+    
+    bool isFull() {
+        return (top == size - 1);
+    }
+};
+
+int main() {
+    char a[] = {'h', 'e', 'l', 'l', 'o', ',', 'w', 'o', 'r', 'l', 'd', '!'};
+    CharStack reverse(12);
+    
+    for (int i = 0; i < 12; i++)
+        reverse.push(a[i]);
+    
+    while (!reverse.isEmpty())
+        cout << reverse.pop();
+    
+    cout << endl;
+}
