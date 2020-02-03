25afdfed67067b1e0ad4f9958c13aec7b9f1f8d9
Anthony Sainez <tsainez@gmail.com>
2020-02-03 13:32:03 -0800

    Update averages.c

--- c/averages.c
+++ c/averages.c
@@ -38,38 +38,4 @@ int main() {
 		printf("Negative average: %d \n", (negTotal / negCount));
 	}
 
-	/*
-	int i;
-	double* p;	// We use this reference variable to access
-			// dynamically created array elements
-
-	p = calloc(10, sizeof(double)); // Make double array of 10 elements
-	
-	for (i=0; i<10; i++)
-		*(p + i) = i;
-	
-	for (i=0; i<10; i++)
-		printf("*(p+%d) = %lf\n", i, *(p+i));
-
-	free(p);
-
-	putchar('\n');
-	
-	p = calloc(4, sizeof(double)); // make a new double array of 4 elements
-
-	// notice the size has changed
-
-	for (i=0; i<4; i++)
-		*(p+i)=i*i; // put value i*i in array element i
-
-	for (i=0; i<4; i++)
-		printf("*(p+%d)=%lf\n", i, *(p+i));
-
-	free(p);
-	*/
-	/*
-	while(number != 0) {
-		printf("Enter a number: ")
-			
-	} */
 }
