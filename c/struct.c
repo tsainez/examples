7db3660d41ae9487b6880298ef7cbf0d9383987c
Anthony Sainez <tsainez@gmail.com>
2020-02-04 23:54:14 -0800

    Create struct.c

---
+++ c/struct.c
@@ -0,0 +1,25 @@
+//
+// struct.c
+//
+// Created by Anthony Sainez on 3 February 2020
+// Copyright © 2020 Anthony Sainez. All rights reserved.
+//
+
+struct point {
+    int x;
+    int y;
+};
+
+void PrintPoint(struct point p) {
+    printf("(%d,%d)\n", p.x, p.y);
+}
+
+int main() {
+    struct point p1 = {0, 10}; // x = 0, y = 10
+    PrintPoint(p1);
+    
+    struct point *p;
+    // the following code is identical
+    printf("x is %d\n", (*p).x);
+    printf("x is %d\n", p->x);
+}
