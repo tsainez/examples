cbf09948d9d7ca7569f8a49f6b38a8bf2ed14494
Anthony Sainez <tsainez@gmail.com>
2020-03-30 17:47:15 -0700

    Create sumAll.s

---
+++ assembly/mips/sumAll.s
@@ -0,0 +1,54 @@
+#
+#  sumAll.s
+#  
+#
+#  Created by Anthony Sainez on 3/30/20.
+#
+
+.data
+
+enter: .asciiz "Enter integer (0 to end): "
+positivePrompt: .asciiz "The sum of the positive integers is: "
+negativePrompt: .asciiz "\nThe sum of the negative integers is: "
+
+
+.text
+
+li $s0, 0 # pos
+li $s1, 0 # neg
+
+loop:
+    li $v0, 4
+    la $a0, enter # prompt
+    syscall
+
+    li $v0, 5
+    syscall # input
+    move $t0, $v0
+    beqz $t0, done
+    bltz $t0, negativeAdd # if x > 0
+    add $s0, $s0, $t0 # is pos
+    j skip
+
+negativeAdd:
+    add $s1, $s1, $t0
+
+skip:
+    j loop
+
+done:
+    li $v0, 4
+    la $a0, positivePrompt # prompt
+    syscall
+
+    move $a0, $s0
+    li $v0, 1
+    syscall
+
+    li $v0, 4
+    la $a0, negativePrompt # prompt
+    syscall
+
+    move $a0, $s1
+    li $v0, 1
+    syscall
