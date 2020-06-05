99149d79caa2b7cac472f5f1dbc2b06ceab2222c
Anthony Sainez <tsainez@gmail.com>
2020-06-05 13:45:41 -0700

    Create goldenratio.m

---
+++ MATLAB/goldenratio.m
@@ -0,0 +1,31 @@
+%% Golden Ratio Approximation
+% Initialize a vector to hold the Fibonacci numbers
+F = zeros(10,1)
+
+% Input the first two Fibonacci numbers 
+F(1) = 1; 
+F(2) = 1;
+
+% The following loop uses a recursive relation to calculate the
+% reamining 8 Fibonacci numbers we need to find.
+for i = 3:10
+    F(i) = F(i-1) + F(i-2);
+end
+
+% Here we create a function handle for
+% the fact that the golden ratio is approximated 
+% by computing the ratio of successive Fibonacci 
+% numbers given as... 
+ri = @(Fi, Fim1) Fi/Fim1; 
+
+% Initialize a vector to hold the ratios for
+% the Fibonacci numbers.
+r = zeros(10, 1);
+
+% Given
+r(1) = 1;
+
+% Computing the ratios using a for loop
+for i = 2:10
+    r(i) = ri(F(i), F(i-1));
+end
