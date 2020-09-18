70a06875dd839cd24a2bb0d6c068aaee81308a81
Anthony Sainez <tsainez@gmail.com>
2020-09-18 05:15:29 -0700

    Create LinearSearch.cpp

---
+++ c++/LinearSearch.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Anthony Sainez on 9/2/20.
+//
+// Sources Used
+//  *   I used the textbook, notes taken during the lectures,
+//      companion material from the textbook. I lowkey just copied
+//      the psuedocode and turned it into C++ code. It wasn't difficult.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int linearSearch(int arr[], int n, int key) {
+    for(int i=0; i < n; i++)
+        if (arr[i] == key)
+            return i;
+    
+    return -1;
+}
+
+int main() {
+    int n, key;
+    cin >> n;
+    cin >> key;
+    
+    int* arr = new int[n];
+    
+    for(int i = 0; i < n; i++)
+    cin >> arr[i];
+    
+    cout << linearSearch(arr, n, key);
+
+    return 0;
+}
