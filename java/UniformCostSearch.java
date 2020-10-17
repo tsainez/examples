a2533a16dc17266cbcfe2f514d5054e21b655d1c
tony <tsainez@gmail.com>
2020-10-16 23:50:29 -0700

    Update UniformCostSearch.java

--- java/UniformCostSearch.java
+++ java/UniformCostSearch.java
@@ -1,11 +1,16 @@
-//Uniform cost search   g(node)
+//
+//  UniformCostSearch
+//
+//  This class implements the uniform cost search algorithm.
+//  It does not use the searchDisplay display utility.
+//  Upon submission, it matches the test logs provided.
+//
+//  Anthony Sainez -- 16 October 2020
+//
+
 import java.util.*;
 
 public class UniformCostSearch {
-    //initialize the frontier with initial/sharing location
-    public SortedFrontier frontier = new SortedFrontier(SortBy.g);
-    //initialize the explored set
-    public Set<String> explore = new HashSet<String>();
     public RoadMap graph;
     public String initialLoc, destinationLoc;
     public int limit, expansionCount;
@@ -15,69 +20,52 @@ public class UniformCostSearch {
         this.graph= graph;
         this.initialLoc= initialLoc;
         this.destinationLoc = destinationLoc;
-        this.limit = limit - 1;
+        this.limit = limit;
     }
 
     // Required Search Function
-    public Node search(boolean checkvertex) {
-        // If the frontier is empty, fail!
-        while(!frontier.isEmpty()) {
-            //chose leaf node and remove from frontier -> fronitier.removeTop()
+    public Node search(boolean stateChecking) {
+        SortedFrontier frontier = new SortedFrontier(SortBy.f);
+        Node iNode = new Node(graph.findLocation(initialLoc));
+        HashSet<String> explore = new HashSet<String>();
+        expansionCount = 0;
+
+        while(!frontier.isEmpty())
             frontier.removeTop();
-        }
 
-        explore.clear();
-        expansionCount =0;
-        Node parent = new Node(graph.findLocation(initialLoc));
+        // We are already here!
+        if(initialLoc == destinationLoc)
+            return iNode;
 
-        if(initialLoc == destinationLoc) {
-            //solution
-            return parent;
-        }
-        frontier.addSorted(parent);
-        explore.add(parent.loc.name);
-        //for each action in problem.ACTIONS(node.STATE)
-        while(!frontier.isEmpty() && parent.depth < limit) {
-            //chose leaf node and remove from frontier
-            parent = frontier.removeTop();
-            //if leaf node is destination, return parent node
-            if(parent.isDestination(destinationLoc)) {
-                return parent;
-            }
-            //explore and expand
-            parent.expand(); expansionCount++;
-            //if the checkvertex is false
-            if(!checkvertex) {
-                frontier.addSorted(parent.children);
-            }
-            //if the checkvertex is true
-            else
-            {
-                for(Node i: parent.children)
-                {
-                    //if child state is not explored or frontier
-                    if(!explore.contains(i.loc.name)) {
-                        //frontier ��INSERT(child,frontier)
+        frontier.addSorted(iNode);
+        explore.add(iNode.loc.name);
+
+        while(!frontier.isEmpty() && iNode.depth < limit) {
+            iNode = frontier.removeTop();
+            if(iNode.isDestination(destinationLoc))
+                return iNode;
+
+            iNode.expand(); expansionCount++;
+
+            if (stateChecking) {
+                for (Node i: iNode.children) {
+                    if (!explore.contains(i.loc.name)) {
                         frontier.addSorted(i);
                         explore.add(i.loc.name);
-                        //if child state is explored or frontier
-                    }else
-                    {
-                        //Fixed error(null exception) by changing explore.contains -> frontier.contains
-                        if(frontier.contains(i.loc.name)) {
-                            //find higher PATH-COST
+                    } else {
+                        if (frontier.contains(i.loc.name)) {
                             Node higher = frontier.find(i);
-                            if(i.partialPathCost <higher.partialPathCost)
-                            {
-                                //replace that frontier node with child
+                            if (i.partialPathCost <higher.partialPathCost) {
                                 frontier.remove(higher);
                                 frontier.addSorted(i);
                             }
                         }
                     }
-                }
+                } // end for (Node i: iNode.children)
+            } else { // stateChecking != true
+                frontier.addSorted(iNode.children);
             }
-        }
+        } // end while(!frontier.isEmpty() && parent.depth < limit)
         return null;
     }
 
