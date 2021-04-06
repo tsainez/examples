ce7e5a634624d46c3d3629a132838ac005d69540
Anthony Sainez <tsainez@gmail.com>
2021-04-05 18:55:11 -0700

    Create lab06_edge.m

---
+++ matlab/lab06_edge.m
@@ -0,0 +1,30 @@
+
+img = im2double(imread('lena.jpg'));
+
+%% compute gradient magnitude and orientation with Sobel filter
+[magnitude, orientation] = sobel_feature(img);
+
+figure, imshow(magnitude); title('Gradient Magnitude');
+figure, imagesc(orientation); colormap jet; axis image; colorbar; title('Gradient Orientation');
+
+imwrite(magnitude, 'lena_sobel_magnitude.jpg');
+h = gcf; saveas(h, 'lena_sobel_orientation.jpg');
+
+
+%% apply thresholding to detect edge
+threshold = 0.4;
+e = magnitude > threshold;
+figure, imshow(e); title(sprintf('Detected edge (threshold = %s)', num2str(threshold)));
+
+imwrite(e, sprintf('lena_edge_threshold_%s.jpg', num2str(threshold)));
+
+
+%% use built-in function to detect edge
+e1 = edge(img, 'Sobel'); % change img to sobel edge detection
+e2 = edge(img, 'Canny'); % change img to canny edge detection
+
+figure, imshow(e1); title('Sobel Edge');
+figure, imshow(e2); title('Canny Edge');
+
+imwrite(e1, 'lena_sobel.jpg');
+imwrite(e2, 'lena_canny.jpg');
