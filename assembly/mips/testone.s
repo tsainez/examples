a45a70670e0a47795fa58dabeabafcfc3a005185
Anthony Sainez <tsainez@gmail.com>
2021-08-20 12:17:48 -0700

    Create testone.s

---
+++ assembly/mips/testone.s
@@ -0,0 +1,26 @@
+# Short test case for your project.
+#
+# Note that this is by no means a comprehensive test!
+#
+
+# Compute the sum of N integers: 1 + 2 + 3 + ... + N
+# In this case, N = 5.
+
+main:
+    # Initialize registers
+    addiu   $t0, $0, 5        # initialize number of integers to be 5
+    addiu    $t1, $0, 0        # initialize counter (i)
+    addiu    $t2, $0 0        # initialize sum
+
+    # Main loop body
+loop:
+    addiu    $t1, $t1, 1    # i = i + 1
+    addu    $t2, $t2, $t1    # sum = sum + i
+    beq    $t0, $t1, exit    # if i = N, continue
+    j    loop
+
+    # Exit routine - sum stored in t2
+exit:
+    addiu    $v0, $0, 10        # exit
+    syscall     # should exit since unsupported
+
