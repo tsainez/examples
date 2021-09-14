cb8b78e2a18e741f31cb9a033c470583fe962df0
Anthony Sainez <tsainez@gmail.com>
2021-09-14 10:53:22 -0700

    Create load-tpch.sql

---
+++ sql/load-tpch.sql
@@ -0,0 +1,19 @@
+.mode "csv"
+.separator "|"
+.import ./data/customer.tbl customer
+.import ./data/lineitem.tbl lineitem
+.import ./data/nation.tbl nation
+.import ./data/orders.tbl orders
+.import ./data/part.tbl part
+.import ./data/partsupp.tbl partsupp
+.import ./data/region.tbl region
+.import ./data/supplier.tbl supplier
+ 
+-- BULK INSERT tpch.customers
+-- FROM 'qwertyuiop'
+-- WITH
+-- (
+--     FIELDTERMINATOR = '|',
+--     ROWTERMINATOR = '\n',
+--     TABLOCK
+-- )
